cf01a5ddbda08ff0070f8f97f078dd73166e2d17
Your Name <you@example.com>
2022-06-16 14:15:51 +0300

    Now the scripts runs in endless loop. But still is raw.

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 from eospy.cleos import Cleos
 import datetime
 import sqlite3
+import time
 
 ce = Cleos(url='https://api.wax.alohaeos.com')
 arguments = {
@@ -14,6 +15,7 @@ db = sqlite3.connect('btk_prices.db')
 # Create cursor
 cursor = db.cursor()
 
+# Create table if don't exist
 cursor.execute("""CREATE TABLE IF NOT EXISTS prices (
      date DATETIME,
      sh_pool INT,
@@ -21,49 +23,23 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS prices (
      btk_price FLOAT
 )""")
 
-# Get the data from blockchain
-get_data = ce.get_table(arguments['code'], arguments['scope'], arguments['table'])
-line_data = get_data['rows'][0]
-sh_pool     = int(line_data['shares_pool'])
-btk_pool    = line_data['tokens_pool']
-price       = btk_pool / sh_pool / 10
-date        = datetime.datetime.now()
 
-#print(f'{date}, {price}, {sh_pool}, {btk_pool}')
+while True:
+    # Get the data from blockchain
+    get_data = ce.get_table(arguments['code'], arguments['scope'], arguments['table'])
+    line_data = get_data['rows'][0]
+    sh_pool = int(line_data['shares_pool'])
+    btk_pool = line_data['tokens_pool']
+    price = btk_pool / sh_pool / 10
+    date = datetime.datetime.now()
 
-cursor.execute('INSERT INTO prices VALUES (?, ?, ?, ?)', (date, sh_pool, btk_pool, price))
+    # print(f'{date}, {price}, {sh_pool}, {btk_pool}')
 
-db.commit()
+    cursor.execute('INSERT INTO prices VALUES (?, ?, ?, ?)', (date, sh_pool, btk_pool, price))
+
+    db.commit()
+
+
+    time.sleep(60)
 
 db.close()
-
-# def animate(i):
-#     data_file = open('prices.csv', 'a')
-#
-#     get_data = ce.get_table(arguments['code'], arguments['scope'], arguments['table'])
-#     line_data = get_data['rows'][0]
-#     sh_pool = int(line_data['shares_pool'])
-#     btk_pool = line_data['tokens_pool']
-#     y = btk_pool / sh_pool * 1000
-#     x = datetime.datetime.now().strftime("%H:%M:%S")
-#     data_file.write(f'{x},{y}\n')
-#     data_file.close()
-#
-#     data_file = open('prices.csv', 'r').read()
-#     lines = data_file.split("\n")
-#     del lines[-1]
-#     xs = []
-#     ys = []
-#
-#     for line in lines:
-#         x, y = line.split(',')
-#         xs.append(x)
-#         ys.append(float(y))
-#
-#     ax.clear()
-#     ax.plot(xs, ys)
-#
-#     plt.xlabel('Date')
-#     plt.ylabel('Price')
-#     plt.title('Realtime Data for BTK price BTK/SH')
-#     print(x, y)
