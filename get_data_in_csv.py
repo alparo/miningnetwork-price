50aef9986470944fc96198d34fe97f2083d6e7d9
Your Name <you@example.com>
2022-06-16 16:10:46 +0300

    Finalizing changes

--- get_data_in_csv.py
+++ get_data_in_csv.py
@@ -11,24 +11,26 @@ arguments = {
     "table": "config"
 }
 
-
 try:
+
     counter = 0
     while True:
         # Get the data from blockchain
         get_data = ce.get_table(arguments['code'], arguments['scope'], arguments['table'])
+        # Prepare data
         line_data = get_data['rows'][0]
         sh_pool = int(line_data['shares_pool'])
         btk_pool = line_data['tokens_pool']
         price = btk_pool / sh_pool / 10
         date = datetime.datetime.now()
 
-        print(f'{date}, {price}, {sh_pool}, {btk_pool}')
+        # Write data to file
         with open('new_prices.csv', "a", newline='') as file:
             writer = csv.writer(file)
             writer.writerow([date, price, sh_pool, btk_pool])
-        counter += 1
 
-        time.sleep(6)
+        counter += 1
+        time.sleep(60)
+
 except KeyboardInterrupt:
-    print(f'Exiting...\nRows added: {counter}')
+    print(f'Exiting...\nEntries added: {counter}')
